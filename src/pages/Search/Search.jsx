a09cf3bd4a46b882dea49552bf68f58372422452
ebuka nathan <ebukanathan@gmail.com>
2023-07-20 23:56:05 +0100

    tv

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { TextField,Box } from "@mui/material"
+import {Button,Tabs,Tab} from "@mui/material"
+import SearchIcon from '@mui/icons-material/Search';
+import { useState } from "react";
+import './Search.css'
 
 function Search() {
+
+    const [type,setType] = useState(0)
   return (
-    <div>
-        <h2>Search</h2>
+    <div style={{alignSelf:'center'}}>
+       <div  className="search-bar">
+       <TextField id="filled-basic" label="Search" variant="filled" />
+       <Button variant="contained"><SearchIcon/></Button>
+       </div>
+       <Tabs value={type} indicatorColor="primary"
+        textColor="primary"
+        style={{paddingBottom:'5px',alignItems:'center',width:10}}>
+        <Tab  style={{width:'50%'}} label="Movies"/>
+        <Tab  style={{width:'50%'}} label="TV Series"/>
+
+       </Tabs>
+
         
     </div>
   )
--- src/pages/TV/Tvshow.jsx
+++ src/pages/TV/Tvshow.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import {useState,useEffect} from 'react'
 import './tv-show.css'
+import MovieCard from '../../components/MovieCard/MovieCard'
+import Custompagination from '../../pagination/custompagination'
 
 function Tvshows() {
+  const [tv,setTv]=useState([]) 
+const [page,setPage] = useState(1)
+
+    const getTv = async()=>{
+        const api  = await fetch(`https://api.themoviedb.org/3/discover/tv?api_key=f663be3cf91abc6255b1a959e84563e8&page=${page}`)
+        const data = await api.json()
+        setTv(data.results)
+        console.log(tv)
+
+    }
+
+    useEffect(()=>{
+        getTv()
+    },[])
+
+ 
   return (
-    <div className='tvshows '>
-        <h1 className='page-title'>Shows</h1>
-    </div>
-  )
-}
+   <>
+    <h1 className="page-title">Top movies</h1>
+    <div className="movies">
+         {
+         tv && tv.map(item=>(
+          <MovieCard key={item.id} image={item.poster_path} name={item.original_title} vote={item.vote_average.toFixed(1)}/> 
+         ))
+        }
+
+         <Custompagination  setPage={setPage}/>
+         
+         </div>
+         </>
+         )}
+
+        
+
 
 export default Tvshows
